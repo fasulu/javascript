0d975c1616a7f1f30fe98e870118c8b3092d6699
unknown <rfasulu@hotmail.com>
2021-03-12 17:00:42 +0100

    Bocule Exercise 1, 2, 3, 04, 05

--- Boucle/forLoop-01.js
+++ Boucle/forLoop-01.js
@@ -2,18 +2,16 @@
 
 // Rappel: le carré d'un nombre est sa valeur, multipliée par lui même. exemple: 5x5 = 25
 
+var temp=0;
 var numStart = 5;
 var numEnd = 10;
-
+var result=0;
 for (i = 5; i <= 10; i++) {
 
-    var result = i * i;
+    result = i * i;
 
     console.log(result);
-
-    if (result == 355) {
-        
-        console.log("Result = " + i + " X " + i);
-    }
+    temp = result +temp;
+    console.log(temp);
 
 }
