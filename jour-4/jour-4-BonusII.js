840db3db36a54fc1d0a12cfd05c79434cff4eea4
unknown <rfasulu@hotmail.com>
2021-03-12 14:34:02 +0100

    Jour4 Ex Bonus II update

--- jour-4/jour-4-BonusII.js
+++ jour-4/jour-4-BonusII.js
@@ -5,17 +5,35 @@ var total = 0;
 var joueur1 = 0;
 var joueur2 = 0;
 
+var temp = 0;
+
+
 launchDice(numberOfDice);
+joueur1 = total;
+launchDice(numberOfDice);
+joueur2 = total;
 
 function launchDice(numberOfDice) {
+    var temp = 0;
+    total = 0;
     var numStart = 0;
     var num = numberOfDice;
-    for (var i = 1; i < (num+1); i++) {
+    for (var i = 1; i < (num + 1); i++) {
 
-        var temp = Math.floor(Math.random()  * (num - numStart + 1) + numStart);
+        temp = Math.floor(Math.random() * (num - numStart + 1) + numStart);
         total = total + temp;
-        // console.log(temp);        
+        console.log(temp);
 
     }
 }
-console.log("total value :" + total);
+
+console.log("gamer1 :" + joueur1);
+console.log("gamer2 :" + joueur2);
+
+if (joueur1 < joueur2) {
+    console.log("Joueur 2 gagnant!" + " Joueur 1 better next time");
+} else if (joueur1 > joueur2) {
+    console.log("Joueur 1 gagnant!" + " Joueur 2 better next time");
+} else if (joueur1 = joueur2) {
+    console.log("Joueur 1 et Joueur 2 DRAW!");
+}
