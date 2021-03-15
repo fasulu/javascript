01ac02f9b4a5ed9f66aa1bca602644654b35b727
unknown <rfasulu@hotmail.com>
2021-03-15 17:27:53 +0100

    JS Revision Ex01

---
+++ revision/revision-01.js
@@ -0,0 +1,19 @@
+// 01 - Alphabet
+
+// - Créer une fonction `sortLetters` qui reçoit une string en paramètre
+// - La fonction retourne une string avec les mêmes lettres, dans l'ordre alphabétique
+// - Appelez votre fonction avec "konexio" comme argument et vérifiez que vous obtenez 
+//  "eiknoox"
+
+
+for(i=0;i<=sentence.length; i++) {
+
+var sentence = [];
+    splitSentence.push(sentence[i]);
+      
+    
+}
+var b = sentence.sort();
+return b.join("");
+​
+console.log(sortLetters("konexio"));
