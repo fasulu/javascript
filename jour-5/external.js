fdcb38f0667ddd673794c5a8a1615dde596e0196
unknown <rfasulu@hotmail.com>
2021-03-16 17:11:37 +0100

    Jour-5 Node NPM Exos

---
+++ jour-5/external.js
@@ -0,0 +1,22 @@
+module.exports = "Hello World";
+
+
+function sayHelloInFrench() {
+    return "Bonjour";
+  }
+  
+  function sayHelloInEnglish() {
+    return "Hello";
+  }
+  
+  module.exports = {
+    sayHelloInFrench,
+    sayHelloInEnglish,
+  };
+
+  exports.sayHelloInFrench = function() {
+    return 'Bonjour';
+  };
+  exports.sayHelloInEnglish = function() {
+    return 'Hello';
+  };
---
+++ jour-5/index.js
@@ -0,0 +1,12 @@
+var example = require("./external.js");
+
+console.log(example);
+
+var {sayHelloInFrench, sayHelloInEnglish} = require("./external.js");
+
+console.log(sayHelloInFrench());
+
+
+// console.log(exemple.sayHelloInFrench());
+// console.log(exemple.sayHelloInEnglish());
+
