362cfd9d3f933998e3ba194f1d1cfa582949be20
unknown <rfasulu@hotmail.com>
2021-03-18 07:25:31 +0100

    Jour5 Guess Game

--- jour-5/play.js
+++ jour-5/play.js
@@ -1,3 +1,14 @@
+// 04 - Guess
+
+// - Installez et importez le package `prompt` (n'oubliez pas d'initialiser !) et lancez les méthodes de `prompt`
+// - Créez une variable `mysteryNum` contenant une valeur aléatoire (un nombre entier entre 1 et 100)
+// - Créez une fonction `play` qui demande au joueur "Quel est le nombre mystère ?" puis :
+//     - si le joueur donne une valeur incorrecte (pas un nombre) on affiche "error" et on relance la fonction `play`
+//     - si le joueur donne un chiffre trop petit, on affiche "C'est plus !" et on relance la fonction `play`
+//     - si le joueur donne un chiffre trop grand, on affiche "C'est moins !" et on relance la fonction `play`
+//     - si le joueur donne le bon nombre, on affiche "Bravo !!"
+// - Appelez la fonction
+// - Testez le jeu
 
 var numStart = 1;
 var numEnd = 100;
