faff87731cd196d2883bde97c9295aeb456edf57
unknown <rfasulu@hotmail.com>
2021-03-22 14:39:13 +0100

    Jour-6-Ex3 completed

--- jour-6/Jour-6-Ex3.js
+++ jour-6/Jour-6-Ex3.js
@@ -1,11 +1,14 @@
 // 03 - Validator
 
 
-// - À l'aide du package `prompt`, créez une fonction `checkProfile` qui demande à l'utilisateur un email, un username et un mot de passe
-// - La fonction vérifiera que l'utilisateur a saisi des informations valides (regardez la doc, et plus precisement les `pattern`) :
+// - À l'aide du package `prompt`, créez une fonction `checkProfile` qui demande à 
+//  l'utilisateur un email, un username et un mot de passe
+// - La fonction vérifiera que l'utilisateur a saisi des informations valides 
+//  (regardez la doc, et plus precisement les `pattern`) :
 //     - l'email doit être au bon format
 //     - le username ne doit comporter que des lettres, chiffres et tirets (les petits)
-//     - le mot de passe doit contenir au moins 6 caractères, au moins une lettre et au moins un chiffre, et peut contenir des tirets
+//     - le mot de passe doit contenir au moins 6 caractères, au moins une lettre et 
+//       au moins un chiffre, et peut contenir des tirets
 // - Si tout est bon, elle retourne "All good !", sinon elle retourne "error"
 
 const prompt = require("prompt");
@@ -16,44 +19,64 @@ function onErr(err) {
     return;
 }
 
+var username = 0;
+var email = 0;
+var password = 0;
+var counter = 0;
+
 prompt.get(
     [
-        "username", 
+        "username",
+        "email",
         "password"
     ],
-    
+
     function (err, res) {
 
         if (err) {
             return onErr(err);
         }
 
-        password = res.passWord;
-        email = res.eMail;
+        username = res.username;
+        email = res.email;
+        password = res.password;
 
-        console.log(typeof email, eMail, password);
+        console.log(typeof username, username, typeof email, email, typeof password, password);
 
-        // var alphaExpression = /^[a-zA-Z1-90-]*$/g
-        //         if(res.username.match(alphaExpression)) {
-        //     console.log("Valid Name");
-        // } else {
-        //     console.log("Invalid Name");
-        // }
+        var alphaExpression = /^[a-zA-Z1-90-]*$/g;  // any number of characters contains atleast one captital or one small or one number or one hyphen
 
-        var emailExpression = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-        if(res.username.match(emailExpression)) {
-            console.log("Valid Username");
+        if (res.username.match(alphaExpression)) {
+            console.log("Valid Name");
+            counter++;
         } else {
-            console.log("Invalid Username");
+            console.log("Invalid Name");
         }
 
-        // var passExpression = /^()$/;
+        var emailExpression = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;  // email verification
 
-        // if(res.password.match(passExpression)) {
-        //     console.log("Valid Password");
-        // } else {
-        //     console.log("Invalid Password");
-        // }
+        if (res.email.match(emailExpression)) {
+            console.log("Valid eMail");
+            counter++;
+        } else {
+            console.log("Invalid eMail");
+        }
 
-    });
+        // var regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,10}$/;  // accept from 6 to 10 characters with atleast one number one small letter, one capital letter, one special character
+
+        var pwdwordExpression = /^(?=.*[0-9])(?=.*[-])[a-zA-Z0-9-]{1,6}$/;      // atleast 6 characters with one number one small letter, one capital letter, one hyphen
+
+        if (res.password.match(pwdwordExpression)) {
+            console.log("Valid Password");
+            counter++;
+        } else {
+            console.log("Invalid Password");
+        }
+
+        if (counter == 3) {
+            console.log("All good !")
+        } else {
+            console.log("error");
+        }
+    }
+);
+
