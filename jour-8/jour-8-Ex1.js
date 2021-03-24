945beff0b7150b525e46255aab6ccd6cc7ea2664
unknown <rfasulu@hotmail.com>
2021-03-24 15:51:19 +0100

    Jour8 Ex1 result with hyphen in console

--- jour-8/jour-8-Ex1.js
+++ jour-8/jour-8-Ex1.js
@@ -17,18 +17,20 @@ request.get("https://restcountries.eu/rest/v1/all",
         //     // console.log(tempArray1[i].name);
         //     nameCountry.push(tempArray1[i].name);
         // }
-        // console.log(nameCountry);  
+        // console.log(nameCountry.join("-"));  
+
         //**************************** */
 
         //***************avec map & call back funciton */
 
         var tempArry2 = tempArray1.map(
             function(elem) {
+                
                 return elem.name;
             }
         );
 
-        console.log(tempArry2);
+        console.log(tempArry2.join("-"));
 
         //******************************************** */  
     }
