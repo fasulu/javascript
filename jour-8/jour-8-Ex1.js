513476bf166101a3964c35a13de58673f5c6d970
unknown <rfasulu@hotmail.com>
2021-03-24 15:39:02 +0100

    Jour8 Ex1

---
+++ jour-8/jour-8-Ex1.js
@@ -0,0 +1,35 @@
+var request = require("request");
+
+var nameCountry = [];
+
+request.get("https://restcountries.eu/rest/v1/all",
+
+    function (err, res, body) {
+
+        var tempArray1 = JSON.parse(body);
+
+        console.log(typeof tempArray1);
+
+        //*****************avec for loop */
+
+        // for(i=0; i<=tempArray1.length-1; i++) {
+
+        //     // console.log(tempArray1[i].name);
+        //     nameCountry.push(tempArray1[i].name);
+        // }
+        // console.log(nameCountry);  
+        //**************************** */
+
+        //***************avec map & call back funciton */
+
+        var tempArry2 = tempArray1.map(
+            function(elem) {
+                return elem.name;
+            }
+        );
+
+        console.log(tempArry2);
+
+        //******************************************** */  
+    }
+);
