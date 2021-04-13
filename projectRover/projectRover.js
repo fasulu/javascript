dbeefe3e82843b0b65646a1f84ca476a2327d203
fasulu <79796718+fasulu@users.noreply.github.com>
2021-04-13 08:20:39 +0200

    Update projectRover.js

--- projectRover/projectRover.js
+++ projectRover/projectRover.js
@@ -1,10 +1,8 @@
-
-// var prompt = require("prompt");
-
-// prompt.start();
+var prompt = require("prompt")
+var beep = require('beepbeep')
 
 var grid = [
-    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    ["N", " ", " ", " ", " ", " ", " ", " ", " ", " "],
     [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
     [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
     [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
@@ -16,18 +14,16 @@ var grid = [
     [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 ];
 
-
-
 var rover = {
     direction: "N",
     x: 0,
-    y: 0
+    y: 0,
+    travelLog: []
 }
 
 function turnLeft(robot) {
 
     switch (robot.direction) {
-
         case "N":
             robot.direction = "W";
             break;
@@ -43,19 +39,18 @@ function turnLeft(robot) {
         case "E":
             robot.direction = "N";
             break;
-
-        // default:
-
     }
-    console.log("The rovor has turned left, its new direction is,", robot.direction);
+
+    grid[robot.y][robot.x] = robot.direction
+
+    // console.log("The rover has turned left, its new direction is:", robot.direction);
 }
 
-// turnLeft(rover);
+// turnLeft(rover)
 
 function turnRight(robot) {
 
     switch (robot.direction) {
-
         case "N":
             robot.direction = "E";
             break;
@@ -71,39 +66,207 @@ function turnRight(robot) {
         case "W":
             robot.direction = "N";
             break;
-
-        // default:
-
     }
-    console.log("The rovor has turned right, its new direction is,", robot.direction);
+
+    grid[robot.y][robot.x] = robot.direction
+
+    // console.log("The rover has turned right, its new direction is:", robot.direction);
 }
 
-// turnRight(rover)
+// turnRight(rover);
 
 function moveForward(robot) {
+    var err = "";
+
+    robot.travelLog.push({
+        x: robot.x,
+        y: robot.y
+    })
 
     switch (robot.direction) {
-
         case "N":
-            robot.y--;
+            if (robot.y === 0) {
+                err = "The rover can't go northern. The rover stays at the same position";
+            } else {
+                robot.y--;
+                // robot.y = robot.y - 1; // pareil que la ligne au dessus
+            }
+
             break;
 
         case "S":
-            robot.y++;
+            if (robot.y === 9) {
+                err = "The rover can't go southern. The rover stays at the same position";
+            } else {
+                robot.y++;
+            }
+
             break;
 
         case "E":
-            robot.x++;
+            if (robot.x === 9) {
+                err = "The rover can't go eastern. The rover stays at the same position";
+            } else {
+                robot.x++;
+            }
+
             break;
 
         case "W":
-            robot.x--;
+            if (robot.x === 0) {
+                err = "The rover can't go western. The rover stays at the same position";
+            } else {
+                robot.x--;
+            }
+
             break;
-
-        // default:
-
     }
-    console.log(`the new position of the rover is x: ${robot.x} and y: ${robot.y}`);
+
+    if (err) {
+        beep(3, 300)
+        console.error(err);
+    } else {
+        var lastPosition = robot.travelLog[robot.travelLog.length - 1]
+        console.log(lastPosition);
+
+        grid[lastPosition.y][lastPosition.x] = " ";
+
+        grid[robot.y][robot.x] = robot.direction;
+
+        // console.log(`The rover has moved forward. The new position of the rover is x: ${robot.x} and y: ${robot.y}`);
+    }
+
 }
 
-moveForward(rover);
+function moveBackward(robot) {
+    var err = "";
+
+    robot.travelLog.push({
+        x: robot.x,
+        y: robot.y
+    })
+
+    switch (robot.direction) {
+        case "S":
+            if (robot.y === 0) {
+                err = "The rover can't go northern. The rover stays at the same position";
+            } else {
+                robot.y--;
+            }
+
+            break;
+
+        case "N":
+            if (robot.y === 9) {
+                err = "The rover can't go southern. The rover stays at the same position";
+            } else {
+                robot.y++;
+            }
+
+            break;
+
+        case "W":
+            if (robot.x === 9) {
+                err = "The rover can't go eastern. The rover stays at the same position";
+            } else {
+                robot.x++;
+            }
+
+            break;
+
+        case "E":
+            if (robot.x === 0) {
+                err = "The rover can't go western. The rover stays at the same position";
+            } else {
+                robot.x--;
+            }
+
+            break;
+    }
+
+    if (err) {
+        beep(3, 300)
+        console.error(err);
+    } else {
+        var lastPosition = robot.travelLog[robot.travelLog.length - 1]
+        console.log(lastPosition);
+
+        grid[lastPosition.y][lastPosition.x] = " ";
+
+        grid[robot.y][robot.x] = robot.direction;
+
+        // console.log(`The rover has moved backwards. The new position of the rover is x: ${robot.x} and y: ${robot.y}`);
+    }
+
+}
+
+// moveForward(rover);
+
+function pilotRover(strInstructions) {
+
+    for (var i = 0; i < strInstructions.length; i++) {
+
+        // console.log(strInstructions.charAt(i));
+
+        switch (strInstructions.charAt(i)) {
+            case "l":
+                turnLeft(rover);
+                break;
+
+            case "r":
+                turnRight(rover);
+                break;
+
+            case "f":
+                moveForward(rover);
+                break;
+
+            case "b":
+                moveBackward(rover);
+                break;
+        }
+    }
+
+    console.table(grid)
+}
+
+
+// pilotRover("rrffflff")
+
+
+// console.table(grid);
+
+function askInstructions() {
+
+    prompt.get(
+
+        ["instructions"],
+
+        function (err, res) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            // console.log(res);
+
+            if (res.instructions === "exit") {
+                console.log("The travel log of the rover is:\n", rover.travelLog);
+                return;
+            } else {
+
+                if (res.instructions.search(/^[lfrb]*$/g) === -1) {
+                    console.log("Please enter a valid command: (l)eft, (r)ight, (f)orward, (b)ackward");
+                    beep()
+                } else {
+                    pilotRover(res.instructions)
+
+                }
+                askInstructions()
+            }
+        }
+    )
+}
+
+console.table(grid);
+askInstructions()
